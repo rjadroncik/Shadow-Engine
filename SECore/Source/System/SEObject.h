bbfd5be06f71e7f04a01e304e589ebc4f96d3271
Roman Jadroncik <roman.jadroncik@gmail.com>
2016-10-20 00:47:42 +0100

    Added metadata to objects.

--- SECore/Source/System/SEObject.h
+++ SECore/Source/System/SEObject.h
@@ -14,11 +14,53 @@
 
 namespace System
 {
+	enum Namespace 
+	{
+		Resources,
+		Rendering
+	};
+
+	class SECORE_API CNamespace : public CObject
+	{
+	public:
+		Namespace Value() _GET { return m_eNamespace; }
+		CString Name() _GET { return STRINGREF(m_Name); }
+
+	private:
+		Namespace m_eNamespace;
+		CString m_Name;
+	};
+
+	enum Category
+	{
+		ParticleSystems
+
+	};
+
+	class SECORE_API CCategory : public CObject
+	{
+	public:
+		Category Value() _GET { return m_eCategory; }
+		CString Name() _GET { return STRINGREF(m_Name); }
+
+	public:
+		CNamespace& Namespace() _GET { return m_Namespace; }
+
+	private: 
+		Category m_eCategory;
+		CString m_Name;
+
+		CNamespace& m_Namespace;
+	};
+
 	class SECORE_API CSEObject : public SCFXML::CXMLObjectSerializable
 	{
 	public:
 		void    ID(_IN CString& rID) _SET { m_ID = rID; }
 		CString ID()                 _GET { return m_ID; }
+
+	public:
+		virtual Category Category() _GET = 0;
 
 	protected:
 		CSEObject() {};
